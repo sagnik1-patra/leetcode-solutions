c2fab01a2c4ee626a8afd702a045b563a9e3e8d6
Sagnik Patra <83433687+sagnik1-patra@users.noreply.github.com>
2025-07-22 16:19:09 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (76.94%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2; // Avoid overflow
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1; // Search in the right half
+            } else {
+                right = mid - 1; // Search in the left half
+            }
+        }
+        
+        // If not found, left is the insertion position
+        return left;
+    }
+};
