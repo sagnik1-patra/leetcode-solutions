acea25fb378b0c270f65596608ba233b07bc518d
Sagnik Patra <83433687+sagnik1-patra@users.noreply.github.com>
2025-07-25 15:49:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (50.92%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x < 2) return x;
+
+        int left = 1, right = x / 2, result = 0;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            long long square = (long long)mid * mid;
+
+            if (square == x) {
+                return mid;
+            } else if (square < x) {
+                result = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
+};
