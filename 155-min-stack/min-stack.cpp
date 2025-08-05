621640bacacfa19c8898cc78b1a14be0229dd2f8
Sagnik Patra <83433687+sagnik1-patra@users.noreply.github.com>
2025-08-05 12:28:12 +0530

    Time: 3 ms (62.00%) | Memory: 23.6 MB (31.66%) - LeetSync

---
+++ 155-min-stack/min-stack.cpp
@@ -0,0 +1,32 @@
+class MinStack {
+private:
+    stack<int> s;
+    stack<int> minStack;
+
+public:
+    MinStack() {
+        // Constructor: no initialization needed beyond the stacks themselves
+    }
+    
+    void push(int val) {
+        s.push(val);
+        if (minStack.empty() || val <= minStack.top()) {
+            minStack.push(val);
+        }
+    }
+    
+    void pop() {
+        if (s.top() == minStack.top()) {
+            minStack.pop();
+        }
+        s.pop();
+    }
+    
+    int top() {
+        return s.top();
+    }
+    
+    int getMin() {
+        return minStack.top();
+    }
+};
