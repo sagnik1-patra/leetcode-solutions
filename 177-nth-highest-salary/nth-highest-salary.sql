912191328fe4f21f4acf6b7d2d45d84a59e6aaff
Sagnik Patra <83433687+sagnik1-patra@users.noreply.github.com>
2025-08-06 09:49:25 +0530

    Time: 428 ms (62.12%) | Memory: 0B (100.00%) - LeetSync

---
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -0,0 +1,12 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  DECLARE offsetVal INT;
+  SET offsetVal = N - 1;
+
+  RETURN (
+      SELECT DISTINCT salary
+      FROM Employee
+      ORDER BY salary DESC
+      LIMIT 1 OFFSET offsetVal
+  );
+END
