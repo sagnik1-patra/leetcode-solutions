47fb6798ca4ac7fc28db31a9d2363447d0f07817
Sagnik Patra <83433687+sagnik1-patra@users.noreply.github.com>
2025-08-08 11:02:07 +0530

    Time: 15 ms (86.17%) | Memory: 50.6 MB (51.68%) - LeetSync

---
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
@@ -0,0 +1,54 @@
+class TrieNode {
+public:
+    TrieNode* children[26];
+    bool isEnd;
+
+    TrieNode() {
+        isEnd = false;
+        for (int i = 0; i < 26; ++i)
+            children[i] = nullptr;
+    }
+};
+
+class Trie {
+private:
+    TrieNode* root;
+
+public:
+    Trie() {
+        root = new TrieNode();
+    }
+
+    void insert(string word) {
+        TrieNode* node = root;
+        for (char ch : word) {
+            int idx = ch - 'a';
+            if (!node->children[idx])
+                node->children[idx] = new TrieNode();
+            node = node->children[idx];
+        }
+        node->isEnd = true;
+    }
+
+    bool search(string word) {
+        TrieNode* node = root;
+        for (char ch : word) {
+            int idx = ch - 'a';
+            if (!node->children[idx])
+                return false;
+            node = node->children[idx];
+        }
+        return node->isEnd;
+    }
+
+    bool startsWith(string prefix) {
+        TrieNode* node = root;
+        for (char ch : prefix) {
+            int idx = ch - 'a';
+            if (!node->children[idx])
+                return false;
+            node = node->children[idx];
+        }
+        return true;
+    }
+};
