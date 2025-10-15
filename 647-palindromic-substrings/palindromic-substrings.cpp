74917ce8f0e96974e4e209f561020c2d7d0f3a4c
Sagnik Patra <83433687+sagnik1-patra@users.noreply.github.com>
2025-10-15 13:19:49 +0530

    Time: 3 ms (89.07%) | Memory: 8.5 MB (92.23%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int n = s.size();
+        int count = 0;
+
+        // Expand around center approach
+        for (int center = 0; center < 2 * n - 1; ++center) {
+            int left = center / 2;
+            int right = left + center % 2;
+
+            // Expand while it's a palindrome
+            while (left >= 0 && right < n && s[left] == s[right]) {
+                count++;
+                left--;
+                right++;
+            }
+        }
+
+        return count;
+    }
+};
