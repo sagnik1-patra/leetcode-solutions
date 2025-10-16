694a4938966201416932bd036d825a5f4ada7d8c
Sagnik Patra <83433687+sagnik1-patra@users.noreply.github.com>
2025-10-16 10:42:12 +0530

    Time: 5 ms (17.59%) | Memory: 10.2 MB (76.04%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0, y = 0;
+        for (char m : moves) {
+            if (m == 'U') y++;
+            else if (m == 'D') y--;
+            else if (m == 'L') x--;
+            else if (m == 'R') x++;
+        }
+        return x == 0 && y == 0;
+    }
+};
