315a15c55955dc0467bdc3723ce0f99a529c1ec3
Sagnik Patra <83433687+sagnik1-patra@users.noreply.github.com>
2025-10-17 11:44:39 +0530

    Time: 3 ms (71.32%) | Memory: 22.2 MB (63.60%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        int left = 0, right = s.size() - 1;
+
+        while (left < right) {
+            if (s[left] != s[right]) {
+                // Try deleting one char from either end
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+private:
+    bool isPalindrome(const string& s, int left, int right) {
+        while (left < right) {
+            if (s[left++] != s[right--]) return false;
+        }
+        return true;
+    }
+};
