2c9d2837d604d27e03642f33928274a6ee6c8506
Sagnik Patra <83433687+sagnik1-patra@users.noreply.github.com>
2025-11-04 13:05:09 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (24.58%) - LeetSync

---
+++ 799-minimum-distance-between-bst-nodes/minimum-distance-between-bst-nodes.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minDiffInBST(TreeNode* root) {
+        int prev = -1;
+        int minDiff = INT_MAX;
+        inorder(root, prev, minDiff);
+        return minDiff;
+    }
+
+private:
+    void inorder(TreeNode* node, int& prev, int& minDiff) {
+        if (!node) return;
+        inorder(node->left, prev, minDiff);
+
+        if (prev != -1)
+            minDiff = min(minDiff, node->val - prev);
+        prev = node->val;
+
+        inorder(node->right, prev, minDiff);
+    }
+};
