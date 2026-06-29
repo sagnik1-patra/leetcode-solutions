a6b86cfba966ee834212abaae69d32642a4a7d4a
Sagnik Patra <83433687+sagnik1-patra@users.noreply.github.com>
2026-06-29 15:36:50 +0530

    Time: 133 ms (26.77%) | Memory: 103.2 MB (24.15%) - LeetSync

---
+++ 1539-diagonal-traverse-ii/diagonal-traverse-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& nums) {
+        map<int, vector<int>> mp;
+
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = 0; j < nums[i].size(); j++) {
+                mp[i + j].push_back(nums[i][j]);
+            }
+        }
+
+        vector<int> ans;
+
+        for (auto& it : mp) {
+            reverse(it.second.begin(), it.second.end());
+
+            for (int x : it.second) {
+                ans.push_back(x);
+            }
+        }
+
+        return ans;
+    }
+};
