4855d24b901fbbe2aa02a1f7c18d71cd7e44c9b9
Sagnik Patra <83433687+sagnik1-patra@users.noreply.github.com>
2026-07-05 20:08:29 +0530

    Time: 136 ms (15.81%) | Memory: 87.2 MB (39.03%) - LeetSync

---
+++ 1581-the-k-strongest-values-in-an-array/the-k-strongest-values-in-an-array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> getStrongest(vector<int>& arr, int k) {
+        sort(arr.begin(), arr.end());
+
+        int n = arr.size();
+        int m = arr[(n - 1) / 2];
+
+        sort(arr.begin(), arr.end(), [m](int a, int b) {
+            if (abs(a - m) == abs(b - m)) {
+                return a > b;
+            }
+            return abs(a - m) > abs(b - m);
+        });
+
+        vector<int> ans;
+        for (int i = 0; i < k; i++) {
+            ans.push_back(arr[i]);
+        }
+
+        return ans;
+    }
+};
