2812407a7e1710f3c713cd951d8332164db2041b
Sagnik Patra <83433687+sagnik1-patra@users.noreply.github.com>
2026-07-20 18:44:46 +0530

    Time: 3 ms (56.00%) | Memory: 61.8 MB (20.80%) - LeetSync

---
+++ 1690-maximum-length-of-subarray-with-positive-product/maximum-length-of-subarray-with-positive-product.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int getMaxLen(vector<int>& nums) {
+        int pos = 0, neg = 0;
+        int ans = 0;
+        
+        for (int x : nums) {
+            if (x > 0) {
+                pos++;
+                neg = (neg > 0) ? neg + 1 : 0;
+            }
+            else if (x < 0) {
+                int newPos = (neg > 0) ? neg + 1 : 0;
+                int newNeg = pos + 1;
+                pos = newPos;
+                neg = newNeg;
+            }
+            else {
+                pos = 0;
+                neg = 0;
+            }
+            
+            ans = max(ans, pos);
+        }
+        
+        return ans;
+    }
+};
