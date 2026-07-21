2183ff7838290a0d413dbc7e8ba8c3931a2b6f69
Sagnik Patra <83433687+sagnik1-patra@users.noreply.github.com>
2026-07-22 00:16:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (52.16%) - LeetSync

---
+++ 1732-minimum-one-bit-operations-to-make-integers-zero/minimum-one-bit-operations-to-make-integers-zero.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minimumOneBitOperations(int n) {
+        int res = 0;
+        
+        while (n) {
+            res ^= n;
+            n >>= 1;
+        }
+        
+        return res;
+    }
+};
