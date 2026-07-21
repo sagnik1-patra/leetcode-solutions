187aebbc0495a90934d1ea75b48502ddd3224aba
Sagnik Patra <83433687+sagnik1-patra@users.noreply.github.com>
2026-07-22 00:16:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (61.21%) - LeetSync

---
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int depth = 0, maxDepth = 0;
+        
+        for (char c : s) {
+            if (c == '(') {
+                depth++;
+                maxDepth = max(maxDepth, depth);
+            }
+            else if (c == ')') {
+                depth--;
+            }
+        }
+        
+        return maxDepth;
+    }
+};
