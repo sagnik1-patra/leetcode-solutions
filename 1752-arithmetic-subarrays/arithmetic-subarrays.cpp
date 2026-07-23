01dab1b3dd4854ae2981a1838ca97f9ceab41715
Sagnik Patra <83433687+sagnik1-patra@users.noreply.github.com>
2026-07-23 20:42:50 +0530

    Time: 33 ms (56.99%) | Memory: 30.1 MB (65.34%) - LeetSync

---
+++ 1752-arithmetic-subarrays/arithmetic-subarrays.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<bool> checkArithmeticSubarrays(vector<int>& nums, vector<int>& l, vector<int>& r) {
+        vector<bool> ans;
+        
+        for (int i = 0; i < l.size(); i++) {
+            vector<int> temp(nums.begin() + l[i], nums.begin() + r[i] + 1);
+            
+            sort(temp.begin(), temp.end());
+            
+            int diff = temp[1] - temp[0];
+            bool ok = true;
+            
+            for (int j = 2; j < temp.size(); j++) {
+                if (temp[j] - temp[j-1] != diff) {
+                    ok = false;
+                    break;
+                }
+            }
+            
+            ans.push_back(ok);
+        }
+        
+        return ans;
+    }
+};
